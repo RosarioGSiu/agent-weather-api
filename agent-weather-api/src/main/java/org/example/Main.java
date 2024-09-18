f0faf6952b440f0ddc1d73f3f447e324e7467236
GregorioMonari <102193750+GregorioMonari@users.noreply.github.com>
2024-09-18 17:10:11 +0200

    feat: added http request code

--- agent-weather-api/src/main/java/org/example/Main.java
+++ agent-weather-api/src/main/java/org/example/Main.java
@@ -1,7 +1,50 @@
 package org.example;
 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+
+        //Perform an HTTP REQUEST
+        URL url = new URL("https://www.youtube.com/");
+        HttpURLConnection connection= (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        //Read results
+        int responseCode= connection.getResponseCode(); //Example (200=success, 404=not found)
+        if(responseCode==200){
+            //YEAH!
+            System.out.println("Success! The http request returned status code: "+responseCode);
+            String response= extractContentFromHttpResponse(connection);
+            System.out.println("Api response:");
+            System.out.println(response);
+        }else{
+            System.out.println("Oh no! The http request failed, status code: "+responseCode);
+        }
+
+        //Close connection
+        connection.disconnect();
     }
+
+    public static String extractContentFromHttpResponse(HttpURLConnection connection) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder content= new StringBuilder();
+
+        //Leggo caratteri finchè non finisco
+        while((inputLine = in.readLine()) != null){
+            System.out.println("Received chunk"+inputLine);
+            content.append(inputLine);
+        }
+
+        in.close();
+        return content.toString();
+    }
+
 }
